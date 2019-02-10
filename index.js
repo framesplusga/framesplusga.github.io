dad144b0b535b2231929c0ebad25c3a5d7651c22
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-02-10 11:34:52 -0600

    Update and rename index.js to script.js

--- index.js
+++
@@ -1,5 +0,0 @@
-function submitUsername() {
-  const usernameBox = document.getElementById("usernameInput");
-  const username = usernameBox.value;
-  window.open(`https://purchase.sk1er.club/checkout/packages/add/3227693/single/gift?username=${username}`, "_self");
-};
